d3f3c2f10c45c6a13d1b2282158e2cd1b4d0ccfd
Wrath866 <61103380+Wrath866@users.noreply.github.com>
2020-02-16 08:29:21 -0800

    Update BotApplet.java

--- BotApplet.java
+++ BotApplet.java
@@ -1,11 +1,34 @@
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class BotApplet extends Applet {
+    @Override
     public Graphics getGraphics() {
-        Graphics g = super.getGraphics();
-        g.setColor(Color.RED);
-        g.drawString("Some string here!", 100, 100);
-        return g;
+        //Initialize TWO images..
+        BufferedImage debugImage = null;
+        if (debugImage == null || debugImage.getWidth() != getWidth() || debugImage.getHeight() != getHeight()) {
+            debugImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        }
+
+        BufferedImage gameImage = null;
+        if (gameImage == null || gameImage.getWidth() != getWidth() || gameImage.getHeight() != getHeight()) {
+            gameImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        }
+
+        //Get the graphics for the DEBUG image..
+        Graphics g = debugImage.getGraphics();
+
+        //Draw gameImage on top of the DEBUG image..
+        g.drawImage(gameImage, 0, 0, null);
+
+        //Draw your own stuff on top of the DEBUG image..
+        g.drawString("Hello World", 50, 50);
+
+        //Draw debug on top of the original graphics..
+        g.dispose();
+        Graphics G = super.getGraphics();
+        G.drawImage(debugImage, 0, 0, null);
+        return gameImage.getGraphics();
     }
 }
